3b0db4b1986331faa627633d8042941d56fbd159
DimaGithubHub <githubhub0@gmail.com>
2024-04-16 11:49:56 +0300

    Комментарий к коммиту

---
+++ FFFF.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+pygame.init()
+
+pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
